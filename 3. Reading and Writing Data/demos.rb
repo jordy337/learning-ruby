2295d0ecce5dbd88b05e1a5ef8dded1933690d0b
jordy337 <jordy337games@gmail.com>
2021-07-20 14:30:16 -0400

    Finish Working With CSV module

--- 3. Reading and Writing Data/demos.rb
+++ 3. Reading and Writing Data/demos.rb
@@ -21,6 +21,7 @@ File.open(file_location, "a") do |file|
 end
 
 # * Reading CSV
+
 require "csv"
 csv_location = "sample.csv"
 
@@ -39,3 +40,41 @@ end
 
 # Method 3 - Clean it up
 csv_parsed = CSV.read(csv_location, headers: true, col_sep: ",") # col_sep specifies CSV delimiter/seperator
+
+# * Writing CSV
+
+data = [
+    ["id", "first_name", "last_name"],
+    [1, "Johh", "Doe"],
+    [2, "Michael", "Smith"],
+    [3, "Sally", "Dean"],
+    [4, "Peter", "Rabbit"],
+    [5, "Jessie", "Fanning"]
+] # CSV formatted array of arrays, what simple CSV.parse returns
+
+# Method 1 - Write as string
+csv_string = data.map{ |d| d.join(",") }.join("\n")
+File.write("demo_generated.csv", csv_string)
+
+# Method 2 - CSV library
+CSV.open("demo_generated.csv", "w") do |c|
+    # c is CSV array of arrays, << operator is to append
+    c << ["id", "first_name", "last_name"]
+    c << [1, "Johh", "Doe"]
+    c << [2, "Michael", "Smith"]
+    c << [3, "Sally", "Dean"]
+    c << [4, "Peter", "Rabbit"]
+    c <<  [5, "Jordyn", "Mindorff"]
+end
+
+# Method 3 - Slightly different, use CSV library to generate CSV string and then write to file
+generated_csv = CSV.generate do |c|
+    c << ["id", "first_name", "last_name"]
+    c << [1, "Johh", "Doe"]
+    c << [2, "Michael", "Smith"]
+    c << [3, "Sally", "Dean"]
+    c << [4, "Peter", "Rabbit"]
+    c <<  [5, "Jordyn", "Murphy Mindorff"]
+end
+
+File.write("demo_generated.csv", generated_csv)
